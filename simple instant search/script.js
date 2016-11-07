89522b16982c4e1848a90d42955508eb92fda058
RomanOriginal <k7ngusha@gmail.com>
2016-11-07 03:02:09 +0200

    Add files via upload

---
+++ simple instant search/script.js
@@ -0,0 +1,71 @@
+// Define a new module for our app
+var app = angular.module("instantSearch", []);
+
+// Create the instant search filter
+
+app.filter('searchFor', function(){
+
+
+
+	return function(arr, searchString){
+
+		if(!searchString){
+			return arr;
+		}
+
+		var result = [];
+
+		searchString = searchString.toLowerCase();
+
+		// Using the forEach helper method to loop through the array
+		angular.forEach(arr, function(item){
+
+			if(item.title.toLowerCase().indexOf(searchString) !== -1){
+				result.push(item);
+			}
+
+		});
+
+		return result;
+	};
+
+});
+
+// The controller
+
+function InstantSearchController($scope){
+
+
+	$scope.items = [
+		{
+			title: '50 Must-have plugins for extending Twitter Bootstrap',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/07/featured_4-100x100.jpg'
+		},
+		{
+			title: 'Making a Super Simple Registration System With PHP and MySQL',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/08/simple_registration_system-100x100.jpg'
+		},
+		{
+			title: 'Create a slide-out footer with this neat z-index trick',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/08/slide-out-footer-100x100.jpg'
+		},
+		{
+			title: 'How to Make a Digital Clock with jQuery and CSS3',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/06/digital_clock-100x100.jpg'
+		},
+		{
+			title: 'Smooth Diagonal Fade Gallery with CSS3 Transitions',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/05/featured-100x100.jpg'
+		},
+		{
+			title: 'Mini AJAX File Upload Form',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/05/ajax-file-upload-form-100x100.jpg'
+		},
+		{
+			title: 'Your First Backbone.js App – Service Chooser',
+			image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/04/service_chooser_form-100x100.jpg'
+		}
+	];
+
+
+}
